742394b4d41b5028eab96fa87cd2c3b901c8f18e
Aayush kumar <78674879+sarraayush@users.noreply.github.com>
2022-01-27 21:29:09 +0530

    main_code for rolling dice uploaded
    date : 27 -01 - 2022

---
+++ dice_rolling.py
@@ -0,0 +1,49 @@
+import random
+print("this is a dice simulator")
+l = "y"
+while l == "y":
+    x = random.randint(1,6)
+    if(x == 1) :
+        print("-----------")
+        print("|         |") 
+        print("|    0    |") 
+        print("|         |") 
+        print("|         |") 
+        print("-----------")
+    if(x == 2) :
+        print("-----------")
+        print("|         |") 
+        print("|    0    |") 
+        print("|         |") 
+        print("|    0    |") 
+        print("-----------")
+    if(x == 3) :
+        print("-----------")
+        print("|         |") 
+        print("|    0    |") 
+        print("|    0    |") 
+        print("|    0    |") 
+        print("-----------")
+    if(x == 4) :
+        print("-----------")
+        print("| 0     0 |") 
+        print("|         |") 
+        print("|         |") 
+        print("| 0     0 |") 
+        print("-----------")
+    if(x == 5) :
+        print("-----------")
+        print("| 0     0 |") 
+        print("|    0    |") 
+        print("|         |") 
+        print("| 0     0 |") 
+        print("-----------")
+    if(x == 6) :
+        print("-----------")
+        print("|  0    0 |") 
+        print("|  0    0 |") 
+        print("|  0    0 |") 
+        print("-----------")
+    l = input("press y to continue and n to stop")
+
+
